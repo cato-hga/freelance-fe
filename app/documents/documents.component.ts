bfb1412e051c1a3aea33ef64c5e5b238dec0039a
JohnCCato <hga813@gmail.com>
2016-11-19 15:21:15 -0500

    Created document objects for document component and view.

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { Document } from './document'
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html'	
 })
 export class DocumentsComponent {
-	
+  documents: Document[] = [
+  	{
+  	  title: "My First Doc",
+      description: 'Describing first document',
+      file_url: 'http://google.com',
+      updated_at: '11/19/16',
+      image_url: 'http://google.com',
+  	},
+    {
+      title: "My Second Doc",
+      description: 'Describing second document',
+      file_url: 'http://google.com',
+      updated_at: '11/19/16',
+      image_url: 'http://google.com',
+    },
+    {
+      title: "My Last Doc",
+      description: 'Describing last document',
+      file_url: 'http://google.com',
+      updated_at: '11/19/16',
+      image_url: 'http://google.com',
+    }
+  ]
 }
